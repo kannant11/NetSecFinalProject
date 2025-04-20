fe541407926d9ad3e98d64a1c7d7691d493567be
tk11 <kannant@merrimack.edu>
2025-04-20 18:53:56 -0400

    let user type in games they want

--- src/Client/clientApplication.java
+++ src/Client/clientApplication.java
@@ -1,15 +1,18 @@
 package Client;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 class clientApplication {
     public static String username; //username
     public static String password; //password
+    public static byte[] games; //list of video games
 
     public static Scanner sc; //scanner so user can type input
 
     //main class
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InputMismatchException, NoSuchElementException, IllegalStateException{
         //user instructed to type in username
         System.out.println("Type in your username: ");
 
@@ -21,5 +24,13 @@ class clientApplication {
 
         //scanner so user can type password
         username = sc.nextLine();
+
+        //user types in games they want
+        System.out.println("Type in the games you want: ");
+
+        //scanner so user can type list of games wanted
+        for (int i = 0; i < args.length; i++) {
+            games[i] = sc.nextByte();
+        }
     }
 }
