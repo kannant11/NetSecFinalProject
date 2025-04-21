2e369cfb36f9d328343f9677203bdaa5b0c7d3ee
nerdynate1 <168118848+nerdynate1@users.noreply.github.com>
2025-04-20 23:26:11 -0400

    Update loadConfig.java

--- src/Server/loadConfig.java
+++ src/Server/loadConfig.java
@@ -1,5 +1,25 @@
 package Server;
 
+import merrimackutil.json.JsonIO;
+import merrimackutil.json.types.JSONObject;
+import java.io.File;
+
 public class loadConfig {
-    
+    private final JSONObject config;
+
+    public loadConfig(String configPath)throws Exception{
+        this.config = JsonIO.readObject(new File(configPath));
+    }
+
+    public String getFilePath(){
+        return config.getString("users File");
+    }
+
+    public String gamesFilePath(){
+        return config.getString("game files");
+    }
+
+    public int getServerPort(){
+        return config.getInt("port");
+    }
 }
