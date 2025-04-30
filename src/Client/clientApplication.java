705ae21b149a29882cd68803d8cc159959331de0
tk11 <kannant@merrimack.edu>
2025-04-30 12:17:25 -0400

    tried to get clientApplication to receive list of games so user can look, opened connection to server, sent username and password to server, closed connection to server

--- src/Client/clientApplication.java
+++ src/Client/clientApplication.java
@@ -1,24 +1,60 @@
 package Client;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.UnknownHostException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-import Server.userManager;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.sound.midi.SysexMessage;
 
+import merrimackutil.json.types.JSONObject;
+
+import Server.userManager;
 import Server.clientHandler;
+import Server.videoGameManager;
 
 public class clientApplication {
     public static String username; //username
     public static String password; //password
-    public static byte[] games; //list of video games
     public static String receivedTOTP; //time-based one-time password received from server
     public static String totp; //time-based one-time password entered by user
+    public static byte[] games; //list of video games
 
     public static Scanner sc; //scanner so user can type input
+    public static SSLSocketFactory factory; //socket factory
+    public static SSLSocket socket; //socket for establishing connection through
+    public static Scanner receiver; //receiver for user to receive information from server
+    public static PrintWriter writer; //writer for user to send information to server
 
     //main class
     public static void main(String[] args) throws InputMismatchException, NoSuchElementException, IllegalStateException{
+        System.setProperty("javax.net.ssl.trustStore", "truststore.jks"); //set up truststore
+        System.setProperty("javax.net.ssl.trustStorePassword", "trust12345"); //set up truststore's password
+
+        //start connection to server
+        try {
+            factory = (SSLSocketFactory) SSLSocketFactory.getDefault(); //socket factory created
+            socket = (SSLSocket) factory.createSocket(); //create socket from its factory
+
+            socket.startHandshake(); //start the handshake process using the socket
+
+            receiver = new Scanner(socket.getInputStream()); //receiver set up
+            writer = new PrintWriter(socket.getOutputStream(), true); //print writer set up
+        } catch (UnknownHostException ue) {
+            ue.printStackTrace(); //print error message if IP address of host is undetermined
+            return; //exception message is returned
+        } catch (IOException ioe) {
+            ioe.printStackTrace(); //print error message if there is issue with input and output streams usage
+            return; //exception message is returned
+        }
+
         //user instructed to type in username
         System.out.println("Type in your username: ");
 
@@ -31,10 +67,16 @@ public class clientApplication {
         //scanner so user can type password
         password = sc.nextLine();
 
+        //send username to server
+        writer.println(username);
+
+        //send password to server
+        writer.println(password);
+
         //if the server verifies that the password is correct
-        if(!clientHandler.verifyPassword(username, password)) {
+        if(clientHandler.verifyPassword(username, password)) {
             //TOTP key client receives from server
-            receivedTOTP = generateTOTPSecret();
+            receivedTOTP = userManager.generateTOTPSecret();
             
             //user instructed to type in totp
             System.out.println("Type in your Time-Based One Time Password: ");
@@ -43,7 +85,13 @@ public class clientApplication {
             totp = sc.nextLine();
 
             //if server verifies the TOTP
-            if (!clientHandler.verifyTOTP(username, totp)) {
+            if (clientHandler.verifyTOTP(username, totp)) {
+                //get the list of games
+                List<JSONObject> gameList = videoGameManager.getAllGames();
+
+                //print list of games
+                System.out.println("Here is the list of games: \n" + gameList);
+
                 //user types in games they want
                 System.out.println("Type in the games you want: ");
 
@@ -52,6 +100,13 @@ public class clientApplication {
                     games[i] = sc.nextByte();
                 }
             }
+        }
+
+        //close connection to server
+        try {
+            socket.close(); //close the connection by disconnecting the socket
+        } catch (IOException ioe) {
+            
         }
     }
 }
