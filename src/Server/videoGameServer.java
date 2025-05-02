32c6357172f2335848bfc7c001b18e693ba9418f
nerdynate1 <168118848+nerdynate1@users.noreply.github.com>
2025-05-01 23:55:06 -0400

    Added 32 byte nonce and multiple negotiations

--- src/Server/videoGameServer.java
+++ src/Server/videoGameServer.java
@@ -6,14 +6,18 @@ import java.net.*;
 import java.util.Arrays;
 
 public class videoGameServer {
+    private static NonceCache nonceChace;
 
     public static void main(String[] args) {
         try {
             loadConfig config = new loadConfig("config.json");
             int port = config.getServerPort();
 
-            userManager userMgr = new userManager("config.json");
-            videoGameManager gameMgr = new videoGameManager("config.json");
+            nonceChace = new NonceChace(32, 300);
+            SessionManager sessionManager = new SessionManager();
+
+            userManager userMgr = new userManager(config.getFilePath());
+            videoGameManager gameMgr = new videoGameManager(config.gamesFilePath());
 
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Server started on port" + port);
@@ -22,10 +26,11 @@ public class videoGameServer {
                 Socket clienSocket = serverSocket.accept();
                 System.out.println("New client connected!");
 
-                clientHandler handler = new clientHandler (clienSocket, userMgr, gameMgr);
-                new Thread(handler).start();
+                /*clientHandler handler = new clientHandler (clienSocket, userMgr, gameMgr);*/
+                new Thread(new clientHandler(clientSocket, userMgr, gameMgr, nonceCache, sessionManager)).start();
             }
         } catch (Exception e) {
+            System.err.println("Server failed.");
             e.printStackTrace();
         }
     }
